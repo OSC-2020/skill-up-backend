3bfd9a7f76136176deee1aae8b8d8d5c5116d838
Rahul Barwal <rahulbarwal654@gmail.com>
2020-12-25 21:47:55 +0530

    Css changes for desired header view.

--- skillup-admin/src/components/layout/header/Header.tsx
+++ skillup-admin/src/components/layout/header/Header.tsx
@@ -7,12 +7,12 @@ export default class Header extends Component {
 
     render() {
         return (
-            <header className="text-gray-600 body-font">
-                <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <header className="text-gray-600 body-font border-b-2">
+                <div className="container mx-auto flex flex-wrap px-5 flex-col md:flex-row items-center">
                     <Link to="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <span className="ml-3 text-xl skillup-text-color-primary">SkillUp-Admin</span>
                     </Link>
-                    <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+                    <nav className="md:mr-auto md:ml-4 md:pl-4 md:border-l md:border-gray-300 flex flex-wrap items-center text-base justify-center">
                         <HeaderTab title="Home" Icon={FaHome} isActive={true} />
                         <HeaderTab title="Books" Icon={FaBook} />
                         <HeaderTab title="Changes" Icon={FaChargingStation} />
--- skillup-admin/src/components/layout/header/HeaderTab.tsx
+++ skillup-admin/src/components/layout/header/HeaderTab.tsx
@@ -13,7 +13,7 @@ export default class HeaderTab extends Component<Props> {
     render() {
         const Icon = this.props.Icon;
         return (
-            <div className={"flex items-center px-3 py-2 mr-1 cursor-pointer transition-colors hover:rounded-md hover:skillup-text-color-primary hover:skillup-background-color-bg " + (this.props.isActive ? "border-b-2 skillup-border-color-primary skillup-text-color-primary" : "")}>
+            <div className={"flex items-center p-3 mr-1 cursor-pointer transition-colors hover:rounded-md hover:skillup-text-color-primary hover:skillup-background-color-bg " + (this.props.isActive ? "border-b-2 skillup-border-color-primary skillup-text-color-primary" : "")}>
                 <Icon />
                 <span className="headerTab__title ml-2">{this.props.title}</span>
             </div>
