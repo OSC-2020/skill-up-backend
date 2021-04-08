fc4e4e9944f97cab88ab624c08da0414bd9c2182
rahulbarwal <rahulbarwal654@gmail.com>
2021-04-08 10:31:43 +0530

    Book detail CRUD operations UI support.

--- skillup-admin/src/components/books/detail/BookDetail.tsx
+++ skillup-admin/src/components/books/detail/BookDetail.tsx
@@ -1,82 +1,118 @@
-import React from 'react';
 import { MobileLayout } from "../../shared/MobileLayout";
 import ScrollToBottom from "../../shared/ScrollToBottom";
 import "./BookDetail.css";
 import ChapterTile from "./ChapterTile";
 
+function deleteChapterFromList(id: number | string) {
+  console.log(
+    "🚀 ~ file: BookDetail.tsx ~ line 9 ~ deleteChapterFromList ~ id",
+    id
+  );
+}
+function moveChapterUpInList(id: number | string) {
+  console.log(
+    "🚀 ~ file: BookDetail.tsx ~ line 13 ~ moveChapterUpInList ~ id",
+    id
+  );
+}
+function moveChapterDownInList(id: number | string) {
+  console.log(
+    "🚀 ~ file: BookDetail.tsx ~ line 17 ~ moveChapterDownInList ~ id",
+    id
+  );
+}
+
+function handleKeyDownOnInput(e: KeyboardEvent) {
+  if (e.key === "Enter") {
+    console.log("do validate");
+  }
+}
 
 export const BookDetail = () => {
-    const chapters = getDummyData();
-    return (
-        <main className="flex py-5 w-full">
-            <nav className="p-3 rounded-md skillup-background-color-bg font-medium shadow-lg w-1/4">
-                <div>Title: </div>
-                <div>Total Chapters: </div>
-            </nav>
-            <section className="w-full ml-5 px-5 flex flex-col items-center">
-                <MobileLayout>
-                    <ScrollToBottom scrollBehaviour="smooth" className="h-full flex flex-col-reverse">
-                        <div className="flex flex-col-reverse overflow-y-auto bookDetail__chapters">
-                            {
-                                chapters.map((chapter, index) => (
-                                    <div>
-                                        <ChapterTile id={chapter.id} title={chapter.title} index={index} />
-                                        <span className="flex rounded my-3 w-full hover:bg-blue-500 h-0.5 bg-blue-200 relative cursor-pointer bookDetail__addChapter"></span>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </ScrollToBottom>
-                </MobileLayout>
-            </section>
-        </main>
-    );
-
-
+  const chapters = getDummyData();
+  return (
+    <main className="flex py-5 w-full">
+      <nav className="p-3 rounded-md skillup-background-color-bg font-medium shadow-lg w-1/4">
+        <div>Title: </div>
+        <div>Total Chapters: </div>
+      </nav>
+      <section className="w-full ml-5 px-5 flex flex-col items-center">
+        <section className="w-72 pb-4 pt-2">
+          <input
+            className="w-full p-2 rounded"
+            placeholder="Add new chapter"
+            onKeyDown={(e: any) => handleKeyDownOnInput(e)}
+          />
+        </section>
+        <MobileLayout>
+          <ScrollToBottom
+            scrollBehaviour="smooth"
+            className="h-full flex flex-col-reverse"
+          >
+            <div className="flex flex-col-reverse overflow-y-auto bookDetail__chapters">
+              {chapters.map((chapter, index) => (
+                <div>
+                  <ChapterTile
+                    id={chapter.id}
+                    title={chapter.title}
+                    index={index}
+                    onDelete={deleteChapterFromList}
+                    onMoveUp={moveChapterUpInList}
+                    onMoveDown={moveChapterDownInList}
+                  />
+                  <span className="flex rounded my-3 w-full"></span>
+                </div>
+              ))}
+            </div>
+          </ScrollToBottom>
+        </MobileLayout>
+      </section>
+    </main>
+  );
 };
 
 function getDummyData() {
-    const arr = [
-        {
-            position: 0,
-            id: "0",
-            title: "string1"
-        },
-        {
-            position: 1,
-            id: "1",
-            title: "number2"
-        },
-        {
-            position: 2,
-            id: "2",
-            title: "var3"
-        },
-        {
-            position: 3,
-            id: "3",
-            title: "let4"
-        },
-        {
-            position: 4,
-            id: "4",
-            title: "const5"
-        },
-        {
-            position: 5,
-            id: "5",
-            title: "let6"
-        },
-        {
-            position: 6,
-            id: "6",
-            title: "const7"
-        },
-        {
-            position: 7,
-            id: "7",
-            title: "let8"
-        }
-    ];
-    return [...arr];
+  const arr = [
+    {
+      position: 0,
+      id: "0",
+      title: "string1",
+    },
+    {
+      position: 1,
+      id: "1",
+      title: "number2",
+    },
+    {
+      position: 2,
+      id: "2",
+      title: "var3",
+    },
+    {
+      position: 3,
+      id: "3",
+      title: "let4",
+    },
+    {
+      position: 4,
+      id: "4",
+      title: "const5",
+    },
+    {
+      position: 5,
+      id: "5",
+      title: "let6",
+    },
+    {
+      position: 6,
+      id: "6",
+      title: "const7",
+    },
+    {
+      position: 7,
+      id: "7",
+      title: "let8",
+    },
+  ];
+  return [...arr];
 }
--- skillup-admin/src/components/books/detail/ChapterTile.tsx
+++ skillup-admin/src/components/books/detail/ChapterTile.tsx
@@ -1,69 +1,96 @@
-import React, { Component } from 'react';
-import { FaTrash } from 'react-icons/fa';
+import React, { Component } from "react";
+import { FaArrowDown, FaArrowUp, FaTrash } from "react-icons/fa";
 import { dynamicCSSClass } from "../../../utilities/string";
 
 interface Props {
-    id: string | number;
-    title: string;
-    index: number;
+  id: string | number;
+  title: string;
+  index: number;
+  onDelete: (id: string | number) => void;
+  onMoveUp: (id: string | number) => void;
+  onMoveDown: (id: string | number) => void;
 }
 
 interface State {
-    currentlyEditing: boolean;
+  currentlyEditing: boolean;
 }
 
-
 export default class ChapterTile extends Component<Props, State> {
-    state = { currentlyEditing: false };
-    view: any;
-    input: HTMLTextAreaElement | undefined;
+  state = { currentlyEditing: false };
+  view: any;
+  input: HTMLTextAreaElement | undefined;
 
-    getHorizMargin = (index: number) => {
-        const pos = index % 4;
-        if (pos === 0) {
-            return "mr-auto";
-        } else if (pos === 1 || pos === 3) {
-            return "mx-auto";
-        }
-        return "ml-auto";
+  getHorizMargin = (index: number) => {
+    const pos = index % 4;
+    if (pos === 0) {
+      return "mr-auto";
+    } else if (pos === 1 || pos === 3) {
+      return "mx-auto";
+    }
+    return "ml-auto";
+  };
+
+  editStart(e: React.MouseEvent) {
+    this.view = e.currentTarget;
+    this.input = document.createElement("textarea");
+    this.input.className = "p-1 border-2 border-gray-100";
+    this.input.value = e.currentTarget.innerHTML;
+
+    this.input.onkeydown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        this.input?.blur();
+      }
     };
 
-    editStart(e: React.MouseEvent) {
-        this.view = e.currentTarget;
-        this.input = document.createElement('textarea');
-        this.input.className = 'p-1 border-2 border-gray-100';
-        this.input.value = e.currentTarget.innerHTML;
+    this.input.onblur = () => {
+      this.editEnd();
+    };
 
-        this.input.onkeydown = (event: KeyboardEvent) => {
-            if (event.key === 'Enter') {
-                this.input?.blur();
-            }
-        };
+    this.view.replaceWith(this.input);
+    this.input.focus();
+  }
 
-        this.input.onblur = () => {
-            this.editEnd();
-        };
+  editEnd() {
+    // TODO: Save data to DB and store here
+    this.view.innerHTML = this.input?.value;
+    this.input?.replaceWith(this.view);
+  }
 
-        this.view.replaceWith(this.input);
-        this.input.focus();
-    }
-
-    editEnd() {
-        // TODO: Save data to DB and store here
-        this.view.innerHTML = this.input?.value;
-        this.input?.replaceWith(this.view);
-    }
-
-    render() {
-        return (
-            <div className={"p-2 rounded-md skillup-background-color-bg font-medium shadow-lg w-2/3 flex items-center justify-between" + (dynamicCSSClass(this.getHorizMargin(this.props.index)))}>
-                {this.props.children}
-                <span onClick={(e) => this.editStart(e)}
-                    className="cursor-pointer w-4/5">
-                    {this.props.title}
-                </span>
-                <FaTrash color="red" className="cursor-pointer text-base w-1/5" />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div
+        className={
+          "p-2 rounded-md skillup-background-color-bg font-medium shadow-lg w-2/3 flex items-center justify-between" +
+          dynamicCSSClass(this.getHorizMargin(this.props.index))
+        }
+      >
+        {this.props.children}
+        <span
+          onClick={(e) => this.editStart(e)}
+          className="cursor-pointer w-4/5"
+        >
+          {this.props.title}
+        </span>
+        {!this.state.currentlyEditing && (
+          <section className="flex items-center justify-between">
+            <FaTrash
+              color="red"
+              className="cursor-pointer text-base w-2/5"
+              onClick={() => this.props.onDelete(this.props.id)}
+            />
+            <span className="flex flex-col items-center w-1/3">
+              <FaArrowUp
+                className="cursor-pointer text-base"
+                onClick={() => this.props.onMoveUp(this.props.id)}
+              />
+              <FaArrowDown
+                className="cursor-pointer text-base"
+                onClick={() => this.props.onMoveDown(this.props.id)}
+              />
+            </span>
+          </section>
+        )}
+      </div>
+    );
+  }
 }
