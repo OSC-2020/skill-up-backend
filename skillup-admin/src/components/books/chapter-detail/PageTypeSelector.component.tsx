4417de27c08f618c14e88c90854b986442ccaeb1
Rahul Barwal <rahulbarwal654@gmail.com>
2021-05-09 07:36:07 +0530

    Toggle switch size configuration.

--- skillup-admin/src/components/books/chapter-detail/PageTypeSelector.component.tsx
+++ skillup-admin/src/components/books/chapter-detail/PageTypeSelector.component.tsx
@@ -7,8 +7,8 @@ interface Props {
 export const PageTypeSelector = (props: Props) => {
     return (
         <main className="flex justify-evenly items-center">
-            <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 pr-8">Content</h2>
-            <ToggleSwitch onSwitchCallback={(val) => console.log(val)} initialVal={false} />
+            <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 pr-8">Theory</h2>
+            <ToggleSwitch onSwitchCallback={(val) => console.log(val)} initialVal={false} size="md"/>
             <h2 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 pl-8">Quiz</h2>
         </main>
     )
--- skillup-admin/src/components/shared/ToggleSwitch.component.tsx
+++ skillup-admin/src/components/shared/ToggleSwitch.component.tsx
@@ -4,13 +4,26 @@ interface Props {
     valueSetColor?: string;
     valueUnSetColor?: string;
     bgColor?: string;
+    size?: 'lg' | 'md' | 'sm';
     onSwitchCallback: (isOn: boolean) => void;
     initialVal: boolean;
+
 }
 
 export const ToggleSwitch = (props: Props) => {
     const [isSet, setIsSet] = useState(props.initialVal || false);
-
+    const size = props.size || 'md';
+    let btnWidth = 16, circleWidth = 8;
+    switch (size) {
+        case 'lg':
+            btnWidth = 20;
+            circleWidth = 10;
+            break;
+        case 'sm':
+            btnWidth = 12;
+            circleWidth = 6;
+            break;
+    }
     function toggleTheme() {
         setIsSet(!isSet);
     }
@@ -19,13 +32,13 @@ export const ToggleSwitch = (props: Props) => {
         props.onSwitchCallback(isSet);
     }, [isSet, props])
 
-    let toggleCircleCss = `w-12 h-12 relative rounded-full transition duration-500 transform p-1 text-white`;
+    let toggleCircleCss = `w-${circleWidth} h-${circleWidth} relative rounded-full transition duration-500 transform p-1 text-white`;
     if (isSet) {
-        toggleCircleCss = `${toggleCircleCss} ${props.valueSetColor || "bg-green-400"} -translate-x-2`;
+        toggleCircleCss = `${toggleCircleCss} ${props.valueSetColor || "bg-green-400"}`;
     } else {
         toggleCircleCss = `${toggleCircleCss} ${props.valueSetColor || "bg-gray-700"} translate-x-full`;
     }
-    const btnCss = `w-20 h-10 rounded-full ${props.bgColor || "bg-gray-300"} flex items-center transition duration-300 focus:outline-none shadow`
+    const btnCss = `w-${btnWidth} h-${circleWidth} rounded-full ${props.bgColor || "bg-gray-300"} flex items-center transition duration-300 focus:outline-none shadow`
     return (
         <button
             className={btnCss} onClick={toggleTheme}>
